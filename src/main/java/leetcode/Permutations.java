d4c50f97f967f57e0654c17fdaf7cd18fade5a82
boileryao <boileryao@gmail.com>
2019-09-02 19:53:01 +0800

    todo permutation

---
+++ src/main/java/leetcode/Permutations.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import static leetcode.LeetCode.Level.MEDIUM;
+
+/**
+ * Created by boileryao on 9/3/2019.
+ * Licensed under WTFPL©2019.
+ */
+public class Permutations {
+    @LeetCode(id=31, problemName = "next-permutation", level =  MEDIUM, accepted = true)
+    public void nextPermutation(int[] nums) {
+
+    }
+}
