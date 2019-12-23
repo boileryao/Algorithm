afe3182e1357b49ddc803288cbf61a1d072a1adf
boileryao <boileryao@gmail.com>
2019-12-24 00:13:50 +0800

    leetcode: majority-element

--- src/main/java/leetcode/MajorityElement.java
+++ src/main/java/leetcode/MajorityElement.java
@@ -9,6 +9,47 @@ import java.util.List;
  * Licensed under WTFPL©2019.
  */
 public class MajorityElement {
+    /**
+     * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+     * <p>
+     * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/majority-element
+     */
+    @LeetCode(id = 169, problemName = "majority-element", level = LeetCode.Level.EASY)
+    public int majorityElement(int[] nums) {
+        int majority = 0;
+        int cnt = 0;
+        for (int num : nums) {
+            if (cnt == 0) {
+                majority = num;
+                cnt++;
+                continue;
+            }
+            if (num == majority) cnt++;
+            else cnt--;
+        }
+
+        return majority;
+    }
+
+    /**
+     * 给定一个大小为 n 的数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。
+     * <p>
+     * 说明: 要求算法的时间复杂度为 O(n)，空间复杂度为 O(1)。
+     * <p>
+     * 示例 1:
+     * 输入: [3,2,3]
+     * 输出: [3]
+     * <p>
+     * 示例 2:
+     * 输入: [1,1,1,3,3,2,2,2]
+     * 输出: [1,2]
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/majority-element-ii
+     */
     @LeetCode(id = 229, problemName = "majority-element-ii", level = LeetCode.Level.MEDIUM)
     public List<Integer> majorityElementII(int[] nums) {
         if (nums == null || nums.length <= 0) return Collections.emptyList();
--- src/test/java/leetcode/MajorityElementTest.kt
+++ src/test/java/leetcode/MajorityElementTest.kt
@@ -8,32 +8,37 @@ import org.testng.annotations.Test
  * Licensed under WTFPL©2019.
  */
 class MajorityElementTest {
-    var solution = MajorityElement()
+    val solution = MajorityElement()
+
     @Test
-    fun testMajorityElementII_Case1() {
+    fun testMajorityElements_Case1() {
         val nums = intArrayOf(3, 2, 3)
         val majorities = solution.majorityElementII(nums)
         Assert.assertEquals(HashSet(majorities), setOf(3))
+        Assert.assertEquals(solution.majorityElement(nums), 3)
     }
 
     @Test
-    fun testMajorityElementII_Case2() {
-        val nums = intArrayOf(1, 1, 1, 3, 3, 2, 2, 2)
+    fun testMajorityElements_Case2() {
+        val nums = intArrayOf(1, 1, 1, 3, 2, 2, 2, 2)
         val majorities = solution.majorityElementII(nums)
         Assert.assertEquals(HashSet(majorities), setOf(1, 2))
+        Assert.assertEquals(solution.majorityElement(nums), 2)
     }
 
     @Test
-    fun testMajorityElementII_Case3() {
+    fun testMajorityElements_Case3() {
         val nums = intArrayOf(1, 1, 1, 1, 1, 1, 1)
         val majorities = solution.majorityElementII(nums)
         Assert.assertEquals(HashSet(majorities), setOf(1))
+        Assert.assertEquals(solution.majorityElement(nums), 1)
     }
 
     @Test
-    fun testMajorityElementII_Case4() {
+    fun testMajorityElements_Case4() {
         val nums = intArrayOf(1, 2, 1, 2, 1)
         val majorities = solution.majorityElementII(nums)
         Assert.assertEquals(HashSet(majorities), setOf(1, 2))
+        Assert.assertEquals(solution.majorityElement(nums), 1)
     }
 }
