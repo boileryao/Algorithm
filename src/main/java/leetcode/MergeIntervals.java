b974aa16b2b6f0f342e1707002bda44851f52a00
boileryao <boileryao@gmail.com>
2020-02-20 22:55:19 +0800

    add: leetcode, merge-intervals

--- src/main/java/leetcode/MergeIntervals.java
+++ src/main/java/leetcode/MergeIntervals.java
@@ -1,8 +1,6 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static leetcode.LeetCode.Level.MEDIUM;
 
@@ -13,6 +11,31 @@ import static leetcode.LeetCode.Level.MEDIUM;
 public class MergeIntervals {
     @LeetCode(id = 56, problemName = "merge-intervals", level = MEDIUM)
     public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) return new int[][]{};
+
+        Arrays.sort(intervals, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            }
+            return o1[0] - o2[0];
+        });
+
+        List<int[]> merged = new ArrayList<>();
+        merged.add(intervals[0]);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] current = intervals[i];
+            int[] lastExpanded = merged.get(merged.size() - 1);
+            if (current[0] <= lastExpanded[1]) {
+                lastExpanded[1] = Math.max(lastExpanded[1], current[1]);
+            } else {
+                merged.add(current);
+            }
+        }
+        return merged.toArray(new int[][]{});
+    }
+
+    @SuppressWarnings("unused")
+    public int[][] merge2(int[][] intervals) {
         Map<Integer, int[]> reflection = new HashMap<>();
         for (int[] interval : intervals) {
             int key = interval[0];
@@ -37,4 +60,7 @@ public class MergeIntervals {
 
         return reflection.values().toArray(new int[][]{});
     }
+    // [[2,3],[4,5],[6,7],[8,9],[1,10]] ==> [[1, 10]]
+    // [[1,4],[5,6]] ==> [[1,4],[5,6]]
+    // [[1,4],[2,3]] ==> [[1,4]]
 }
