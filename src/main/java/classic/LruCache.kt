c9cfc90253eaacb9d02ac78b87270b8b9e4a9714
boileryao <boileryao@gmail.com>
2020-03-06 23:15:59 +0800

    add: classic, lru cache

--- src/main/java/classic/LruCache.kt
+++ src/main/java/classic/LruCache.kt
@@ -3,89 +3,40 @@ package classic
 import java.util.*
 
 class LruCache<K : Any, V : Any>(private val capacity: Int) {
-    private var used: Int = 0
+    private val cacheMap = mutableMapOf<K, CacheNode<K, V>>()
 
-    private val cacheMap = LinkedHashMap<K, CacheNode<K, V>>()
-    private var dualList: CacheNode<K, V>? = null
+    /*
+     * We are assuming the build-in list performs get/remove in O(1) for convenience
+     */
+    private var dualList = LinkedList<CacheNode<K, V>>()
 
     init {
         if (capacity < 1) throw IllegalArgumentException("This capacity is not acceptable")
-        this.used = 0
     }
 
     fun add(key: K, value: V) {
-        // empty
-        if (dualList == null) {
-            val head = CacheNode<K, V>()
-            head.key = key
-            head.value = value
-            head.pre = head
-            head.next = head
-            cacheMap[key] = head
-            dualList = head
-            used++
-            return
+        val cacheNode = CacheNode(key, value)
+        if (dualList.size == capacity) {
+            remove(if (cacheMap.contains(key)) key else dualList.last.key)
         }
-
-        // full
-        if (used == capacity) {
-            val last = dualList!!.pre
-            cacheMap.remove(last.key)
-            last.key = key
-            last.value = value
-            cacheMap[key] = last
-            return
-        }
-
-        // else
-        val node = CacheNode<K, V>()
-        node.key = key
-        node.value = value
-        node.pre = dualList!!.pre
-        node.next = dualList!!
-        node.pre.next = node
-        dualList!!.pre = node
-        used++
-        dualList = node
-        cacheMap[key] = node
+        dualList.addFirst(cacheNode)
+        cacheMap[key] = cacheNode
     }
 
-    operator fun get(key: K): V? {
-        add(key, remove(cacheMap[key] ?: return null)!!.value)
-        println(cacheMap.values.joinToString())
-        return cacheMap[key]!!.value
+    fun get(key: K): V? {
+        val value = cacheMap[key] ?: return null
+
+        dualList.remove(value)
+        dualList.addFirst(value)
+        return value.value
     }
 
     fun remove(key: K): V? {
-        val nodeToRemove = remove(cacheMap[key] ?: return null)
-        return nodeToRemove?.value
+        cacheMap[key]?.let {
+            dualList.remove(cacheMap.remove(key))
+            return it.value
+        } ?: return null
     }
 
-    private fun remove(node: CacheNode<K, V>): CacheNode<K, V>? {
-        dualList ?: return null
-        if (used == 1 && dualList == node) {
-            dualList = null
-            return node
-        }
-
-        val pre = node.pre
-        val next = node.next
-        pre.next = next
-        next.pre = pre
-        return node
-    }
-
-    private class CacheNode<K : Any, V : Any> {
-        lateinit var key: K
-        lateinit var value: V
-        lateinit var pre: CacheNode<K, V>
-        lateinit var next: CacheNode<K, V>
-
-        override fun toString(): String {
-            return "CacheNode{" +
-                    "key=" + key +
-                    ", value=" + value +
-                    '}'.toString()
-        }
-    }
+    private class CacheNode<K : Any, V : Any>(var key: K, var value: V)
 }
