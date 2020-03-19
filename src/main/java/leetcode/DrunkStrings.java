f2247d12766ab417c634d57f3c271808ffec4b01
boileryao <boileryao@gmail.com>
2020-03-19 22:52:01 +0800

    add: leetcode, find-words-that-can-be-formed-by-characters

--- src/main/java/leetcode/DrunkStrings.java
+++ src/main/java/leetcode/DrunkStrings.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * Created by boileryao on 2020/3/12.
  * Licensed under WTFPL©2020.
@@ -16,6 +18,39 @@ public class DrunkStrings {
             if (remaining == null) return "";
             if (remaining.isEmpty()) return str2;
         } while (true);
+    }
+
+    @LeetCode(id = 1160, problemName = "find-words-that-can-be-formed-by-characters", level = LeetCode.Level.EASY)
+    public int countCharacters(String[] words, String chars) {
+        char[] charArray = chars.toCharArray();
+        Arrays.sort(charArray);
+
+        int knowledge = 0;
+        for (String word : words) {
+            if (word.length() > charArray.length) continue;
+            char[] wordArray = word.toCharArray();
+            Arrays.sort(wordArray);
+            int legoIdx = 0;
+            int wordIdx;
+            out:
+            for (wordIdx = 0; wordIdx < word.length(); wordIdx++) {
+                if (legoIdx >= charArray.length) {
+                    break;
+                }
+                while (wordArray[wordIdx] != charArray[legoIdx]) {
+                    legoIdx++;
+                    if (legoIdx >= charArray.length) {
+                        break out;
+                    }
+                }
+                legoIdx++;
+            }
+            if (wordIdx == word.length()) {
+                knowledge += word.length();
+            }
+        }
+
+        return knowledge;
     }
 
     private String remaining(String a, String b) {
--- src/test/java/leetcode/DrunkStringsTest.java
+++ src/test/java/leetcode/DrunkStringsTest.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -24,5 +25,58 @@ public class DrunkStringsTest {
     @Test
     public void testGcdOfStrings_Case3() {
         assertEquals(drunkStrings.gcdOfStrings("LEET", "CODE"), "");
+    }
+
+    @Test
+    public void testCountCharacters_1() {
+        String[] dict = {"cat", "bt", "hat", "tree"};
+        Assert.assertEquals(drunkStrings.countCharacters(dict, "atach"), 6);
+    }
+
+    @Test
+    public void testCountCharacters_2() {
+        String[] dict = {"hello", "world", "leetcode"};
+        Assert.assertEquals(drunkStrings.countCharacters(dict, "welldonehoneyr"), 10);
+    }
+
+    @Test
+    public void testCountCharacters_Fail1() {
+        String[] dict = {
+                "pxlqovnbsgvqjzpfeidchzrodnbqfrccfydzjptukscjuatlsrcurepllxzyghhdepitqptlmfkhzxjgswaulxkfydhkilg",
+                "uqklvqnlhdkwryjawkqfajfpbcnhglxlwxlaskxlytr", "jvgkxcdkxrvfahjkvhmfuyjzxtyxrsogbtsjybeaxugqymcr",
+                "rgxditmosplnqvodtis", "jm",
+                "ruqjwejuanjtiizcmbieobesnjnadzqvqumggblzmkxilgfarnxwpcawtkzxlvugibpidvwtikloeziuxqoi",
+                "wxeyzrnbhlhwxecrgejsrawyulynvgtszwqqlihkcvckgcgtgpyqtkuwvjywmhpskaiwmpyarftqhnogxpith",
+                "vdpbykqlihtpvfnqbrcjpggojqbalerohyitktuikbffvfatcnneuvbanjihiaorrjcdthntnrxebfhvshmpodmzhtwnasbm",
+                "wgjstkoaojcesfdrllugmojwdmgeejyiqvshlowtktddattunarnohgvqsoskfmbrami",
+                "ecwqxbawirvnxvsjybbebclaturorlcbpf",
+                "gtjbaigvufrotlwfoqqolnjabnvtbcygtfcytinzpcjbvprdkdjawrcbthmxjrabimhhs",
+                "cvzlbrvppkyxtjxzeglzwoagmpjnfxddbwolxmwdohgtfktgftzzkwpianslkpldyfzubtjczse", "neaw",
+                "mxhmvkajofnmdiiduactlemcvpztscmsnrdhuhquxnnzywsrzxyplgbppiypxwcfbsnyzblaeifo",
+                "krekecabfpufucjhqphjnibaeqdvdpmrfougdwugvoioqangdnxromwlxnfeydaneyazzclscqgdxlhhgnoqmslfflzqv",
+                "klutvchxsceihfmzitgqakytesfjykribjhjzdruuoycjkwaghmmqkfrhkrtawudtjyjwqbyspamlegqjlwlj",
+                "raykfkflqdzrpthdejetwolgciygwaktulkxemkdbbllkjxhnnafsms", "os",
+                "xhchkcetmlprcdmrnalvkvgabxxnomphqpqhnddqhecogspbdebeoshvjgzvmqu",
+                "jqtdysnpskktynxwmsfaabglagnqcllptvuyyqjwrmqaftenusmsahhhqubkwxltpr",
+                "sulmwluiwvlroldpiyecaicwrawzwflokefqkdwmxejaovvpbflfdtviinbvvtlhhhefmgfsqs", "sxyhcckvyl",
+                "vsaacsybcddxvuzkddjmuivsvtjyanpbydmkcwnkmxvsiantgkvkmqjysclsvd", "sxcghypulvmfqfunxj",
+                "pozekufhlooosxpcggbi", "xzaoxzllcixfqbupqozmzrnugj", "j", "tgslwp",
+                "ntrdkixexajlpjgmcbrqimwtqnzfrqjszeiuvrgzclerzmsnagzaxbredvlrmipzflrzusclckmxphc",
+                "ifdflsywdfizpodsehrrifsvejcxarrxmxyjgbbvqyqvywncphzfmnxhybxpdcozfnzablfx",
+                "uluhplzrsaehaqxfnbcmixueedevhirbwqdyztwaxnkogcauymxgcpabxekib",
+                "agtfkinbdccoemxetbryzpluzlpyzicnfopphrxriqm",
+                "pdympxpwvxwcqaxrvbvyqkrresrjgzgxuyfxtkgldtathokdbyfsqfmitmiyagtqgijaiazvsumeyutbbwxqdshquzrehn",
+                "rqe", "sljsvenhhstnnngzqyo", "dezrzpgldeimxfoqajuhjctgvalwkhkjemjaxumxqmifglbizusuqlttxirpbqbuvauwy",
+                "dkwpyezbmkcskoxxcgrxcewknqgdckjxfyzcmzqcbyeucxbqybxoldogtkmdknsrruvnlfqnpfx",
+                "sjeftmjkuperfynbreycwhuoyqybticswblbrrpugzhlrmiedjqufnehevzqwtaebwvdswsz",
+                "lolnfyliloqmhjmhhohdtgfajjfdjqpubslbsrwitbjmrcegdrdjzvonxaefdvdfcbqmaaks",
+                "qhcoaiocjhuzywkirlblajgeapzajqsoa",
+                "sxrmoqxqbtakuqwmrrkljmegbwbeqbywmlyuprwyhljzejbybxoumidpxdrohwdjoqycpxcmivaulnqzneydwqfsvcxgyyseuk",
+                "yrowy", "dohrzkrzdjehpctnjrvhzojullsiucrhphshwxwicyzkvzbqrztic",
+                "rmshnxtbhsdgkiijrmwulocdzjzpgfyimkjdthuzkhoqgkeawgwincubrloknocxwzgqqcxafksxgzh",
+                "aymovnuhptozhkiyowbeguzlkmrwjnujgjbdne", "abc", "vxoigovwmqizmkwbkktqk",
+                "uokwktdempzloaglvvkqstztmwzcmhgxtoua", "bzwjxqueazlzfojrkbqmhtwrvuwsnfcnylurnddpektekca",
+                "qgndjgvzcyajhgmrrnhdywwdbmkhtthwcfiueuxfldanyrmccwcy"};
+        Assert.assertEquals(drunkStrings.countCharacters(dict, "figspbov"), 2);
     }
 }
