6c0961f6f1ef59dbb02a06f796ac6146c01d5bae
boileryao <boileryao@gmail.com>
2020-03-28 16:24:00 +0800

    add: leetcode, invert-binary-tree

--- src/main/java/leetcode/TreeOperations.java
+++ src/main/java/leetcode/TreeOperations.java
@@ -2,9 +2,12 @@ package leetcode;
 
 import model.TreeNode;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
+import static leetcode.LeetCode.Level.EASY;
 import static leetcode.LeetCode.Level.MEDIUM;
 
 /**
@@ -18,6 +21,23 @@ public class TreeOperations {
         List<Integer> result = new ArrayList<>();
         midOrderIterate(result, root);
         return result;
+    }
+
+    @LeetCode(id = 226, problemName = "invert-binary-tree", level = EASY)
+    public TreeNode invertTree(TreeNode root) {
+        Queue<TreeNode> wfq = new ArrayDeque<>();
+        if (root != null) wfq.add(root);
+
+        while (wfq.size() > 0) {
+            TreeNode current = wfq.remove();
+            TreeNode tmp = current.left;
+            current.left = current.right;
+            current.right = tmp;
+            if (current.left != null) wfq.add(current.left);
+            if (current.right != null) wfq.add(current.right);
+        }
+
+        return root;
     }
 
     private void midOrderIterate(List<Integer> result, TreeNode root) {
